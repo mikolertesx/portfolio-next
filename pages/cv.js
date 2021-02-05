c35c5002cade51e30668b3838de4dfd47ea4fe86
Mike <mgangsal@outlook.com>
2021-02-05 11:55:38 -0600

    Created place holder pages

---
+++ pages/cv.js
@@ -0,0 +1,5 @@
+const Cv = () => {
+	return <h1>Curriculum Vitae page!</h1>
+}
+
+export default Cv;
---
+++ pages/portfolios.js
@@ -0,0 +1,5 @@
+const Portfolios = () => {
+	return <h1>Portfolios page!</h1>
+}
+
+export default Portfolios;
